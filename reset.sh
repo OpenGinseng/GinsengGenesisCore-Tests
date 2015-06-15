a3674157c14abe2ef756e77dead0bbee724ffcb4
Christoph Dorn <christoph@christophdorn.com>
2015-06-15 16:06:21 -0700

    wip

---
+++ reset.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Source https://github.com/cadorn/bash.origin
+. "$HOME/.bash.origin"
+function init {
+	eval BO_SELF_BASH_SOURCE="$BO_READ_SELF_BASH_SOURCE"
+	BO_deriveSelfDir ___TMP___ "$BO_SELF_BASH_SOURCE"
+	local __BO_DIR__="$___TMP___"
+
+
+	rm -Rf ".deps" > /dev/null || true
+	rm -Rf ".pinf" > /dev/null || true
+	rm -Rf ".pgs" > /dev/null || true
+	rm -Rf "boot" > /dev/null || true
+	rm -Rf *.local.json > /dev/null || true
+
+}
+init $@
