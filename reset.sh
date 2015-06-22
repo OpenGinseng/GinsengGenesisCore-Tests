080ce1f9aecbd44b5b809363eabe890c10d79311
Christoph Dorn <christoph@christophdorn.com>
2015-06-21 23:07:36 -0700

    wip

--- reset.sh
+++ reset.sh
@@ -17,5 +17,14 @@ function init {
 	rm -Rf "boot" > /dev/null || true
 	rm -Rf *.local.json > /dev/null || true
 
+	rm -Rf ".cleanignore" > /dev/null || true
+	rm -Rf ".ditignore" > /dev/null || true
+	rm -Rf "io.ginseng.genesis.open.core.tests.inf" > /dev/null || true
+	rm -Rf "io.ginseng.genesis.open.core.tests.js" > /dev/null || true
+	rm -Rf "program.json" > /dev/null || true
+	rm -Rf "smi.json" > /dev/null || true
+	rm -Rf "vortex.js" > /dev/null || true
+	rm -Rf "WORKSPACE.md" > /dev/null || true
+
 }
 init $@
